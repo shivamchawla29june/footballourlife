fe19de7a8ac73b8413f35148e67f888153fce871
shivam chawla <shivamchawla.29june@gmail.com>
2017-08-27 17:46:06 +0530

    adding new files

---
+++ jsfiles/swap.js
@@ -0,0 +1,16 @@
+function first(){
+    document.getElementById("showdata").innerHTML=
+
+    }
+function second(){document.getElementById('showdata').innerHTML= ' 
+;' 
+}
+function third(){
+document.getElementById('showdata').innerHTML="    
+   
+";
+}
+function fourth() {
+    document.getElementById("showdata").innerHTML="<p>234234234234234</p>";
+    }
+
